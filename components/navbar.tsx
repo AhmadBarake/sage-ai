075fa0b6a7b4f5e434a27a19b6073d0e3447a728
ahmad.barake <ahmad.barake@chk-mate.com>
2023-09-13 00:31:00 +0300

    started on sidebar nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,7 +16,7 @@ const font = Poppins({
 
 export const NavBar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
         <div className="flex items-center">
             <Menu className="block md:hidden "/>
             <Link href="/"><h1 className={cn("hidden md:block text-xl md:text-3xl font-bold text-primary", font.className)}>Sage AI</h1></Link>
