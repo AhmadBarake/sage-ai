72bbb0a871fad18b258bcd93287f7d7e25d472b3
ahmad.barake <ahmad.barake@chk-mate.com>
2023-09-14 05:31:34 +0300

    building create sage form

--- app/(root)/(routes)/sage/[sageId]/page.tsx
+++ app/(root)/(routes)/sage/[sageId]/page.tsx
@@ -1,8 +1,27 @@
-const sageIdPage = () => {
+import prismadb from "@/lib/prismadb";
+import { SageForm } from "./components/sage-form";
+
+interface sageIdPageProps {
+    params: {
+        sageId: string;
+    }
+};
+
+const sageIdPage = async ({
+    params
+}: sageIdPageProps) => {
+    //TODO: Check subscription
+
+    const sage = await prismadb.sage.findUnique({
+        where: {
+            id: params.sageId,
+        }
+    })
+
+    const categories = await prismadb.category.findMany();
+
     return (
-        <div>
-            Hello Sage ID
-        </div>
+        <SageForm initialData={sage} categories={categories} />
     )
 }
 
